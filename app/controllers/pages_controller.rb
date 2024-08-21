66038b280d84070f0a6611e537d504c79da49a91
rachit <rachitkumar1111@gmail.com>
2024-08-21 16:20:07 +0530

    sub_string_prolem solution!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,4 +16,18 @@ class PagesController < ApplicationController
 	end
 	output = caesar_cipher("What a string!", 5)
 	puts output  # => "Bmfy f xywnsl!"	
+
+	def substrings(word, dictionary)
+	  result = Hash.new(0)
+	  word = word.downcase
+	  dictionary.each do |substring|
+	    substring = substring.downcase
+	    matches = word.scan(substring).length
+	    result[substring] += matches if matches > 0
+	  end
+	  result
+	end
+	dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+	puts substrings("below", dictionary)
+	puts substrings("Howdy partner, sit down! How's it going?", dictionary)
 end
