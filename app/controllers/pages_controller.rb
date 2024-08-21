e7b7a26cd8d735707c250989e10bce643584dbfe
rachit <rachitkumar1111@gmail.com>
2024-08-21 15:43:28 +0530

    caesar_cipher problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
-	def home
+	def caesar_cipher(text, shift)
+	  result = ""
+	  text.each_char do |char|
+	    if char.match(/[A-Z]/)
+	      shifted_char = ((char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
+	      result += shifted_char
+	    elsif char.match(/[a-z]/)
+	      shifted_char = ((char.ord - 'a'.ord + shift) % 26 + 'a'.ord).chr
+	      result += shifted_char
+	    else
+	      result += char
+	    end
+	  end
+	  result
 	end
+	output = caesar_cipher("What a string!", 5)
+	puts output  # => "Bmfy f xywnsl!"	
 end
