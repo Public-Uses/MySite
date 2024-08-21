696de83eb8c12267c0e063a5273823afec632d69
rachit <rachitkumar1111@gmail.com>
2024-08-21 16:47:19 +0530

    stock_picker_prolem solution!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -30,4 +30,24 @@ class PagesController < ApplicationController
 	dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 	puts substrings("below", dictionary)
 	puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+	def stock_picker(prices)
+	  min_price = prices[0]
+	  min_day = 0
+	  max_profit = 0
+	  best_days = [0, 0]
+	  prices.each_with_index do |price, day|
+	    if price < min_price
+	      min_price = price
+	      min_day = day
+	    end
+	    profit = price - min_price
+	    if profit > max_profit
+	      max_profit = profit
+	      best_days = [min_day, day]
+	    end
+	  end
+	  best_days
+	end
+	puts stock_picker([17,3,6,9,15,8,6,1,10]).inspect
 end
